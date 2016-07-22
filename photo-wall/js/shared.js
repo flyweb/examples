f6bcc0c35604a71cc74646035fcc557817a98849
Justin D'Arcangelo <justindarc@gmail.com>
2016-07-22 13:26:35 -0400

    Added ability to zoom in on the photos on the wall.

--- photo-wall/js/shared.js
+++ photo-wall/js/shared.js
@@ -1,7 +1,48 @@
 var photoStore = new PhotoStore();
 
 $(() => {
-  var $cardColumns = $('#card-columns');
+  var $body          = $(document.body);
+  var $cardColumns   = $('#card-columns');
+  var $fixedElements = $('.fixed-bottom');
+
+  var isZoomed = false;
+  var isZooming = false;
+
+  $cardColumns.on('click', 'a', (evt) => {
+    evt.preventDefault();
+
+    if (isZooming) {
+      return;
+    }
+
+    isZooming = true;
+
+    if (isZoomed) {
+      zoom.out({
+        callback() {
+          $body.removeClass('zoom');
+          $fixedElements.fadeIn(100);
+
+          isZoomed = false;
+          isZooming = false;
+        }
+      });
+    }
+
+    else {      
+      $fixedElements.fadeOut(100, () => {
+        $body.addClass('zoom');
+
+        zoom.to({
+          element: evt.target,
+          callback() {
+            isZoomed = true;
+            isZooming = false;
+          }
+        });
+      });
+    }
+  });
 
   photoStore.addEventListener('add', (evt) => {
     var photo = photoStore.get(evt.id);
