967dbdac72a82d5d7d29151779d93ec93ae99c23
Justin D'Arcangelo <justindarc@gmail.com>
2016-11-14 16:27:27 -0500

    Added NPM dependencies for node-flyweb-aio-printer.

--- node-flyweb-aio-printer/app.js
+++ node-flyweb-aio-printer/app.js
@@ -19,6 +19,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
